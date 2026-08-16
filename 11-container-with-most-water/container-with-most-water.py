46945da2f2555f60ab0f048502f9ccac5a71258d
Akshat Bijalwan <165464102+NemesisWaVe@users.noreply.github.com>
2026-08-16 20:05:18 +0530

    Time: 67 ms (10.54%) | Memory: 29.6 MB (37.60%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,21 +1,15 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        l, r = 0, len(height) - 1
-        res = 0 # Keeps track of the biggest area found so far
-
-        while l < r:
-            # 1. Calculate Area for current window
-            Width = (r - l)
-            Height = min(height[l], height[r])
-            area = (r - l) * min(height[l], height[r])
-            
-            # 2. Update max result
-            res = max(res, area)
-
-            # 3. GREEDY MOVE: Move the shorter pointer inward
-            if height[l] < height[r]:
-                l += 1
+        f=0
+        r=len(height)-1
+        maxWater=0
+        while f<r:
+            width=r-f
+            currHeight=min(height[f],height[r])
+            currArea=width*currHeight
+            maxWater=max(currArea,maxWater)
+            if height[f]<height[r]:
+                f+=1
             else:
                 r-=1
-                
-        return res
+        return maxWater
