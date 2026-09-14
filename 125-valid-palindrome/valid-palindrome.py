c93da93639f5acb3e46e14a2af65721da152d596
Akshat Bijalwan <165464102+NemesisWaVe@users.noreply.github.com>
2026-09-14 20:27:44 +0530

    Time: 7 ms (80.71%) | Memory: 19.5 MB (94.45%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,22 +1,16 @@
+from collections import deque
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         l=0
         r=len(s)-1
         while l<r:
-            while l<r and not self.alphaNum(s[l]):
+            while l<r and not s[l].isalnum():
                 l+=1
-            while l<r and not self.alphaNum(s[r]):
+            while l<r and not s[r].isalnum():
                 r-=1
-            if s[l].lower()==s[r].lower():
-                l+=1
-                r-=1
-            else:
+            if s[l].lower()!=s[r].lower():
                 return False
+            l+=1
+            r-=1
         return True
-    def alphaNum(self,c):
-        val=ord(c)
-        return (ord('A')<=val<=ord('Z') or
-               ord('a')<=val<=ord('z') or
-               ord ('0')<=val<=ord('9'))
-
-            
+        
